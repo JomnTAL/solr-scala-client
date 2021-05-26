8d71846ddd3aa1705bd07f686ef4fb72ab8e858f
Jeremy Auguste <jeremy.auguste@protonmail.com>
2021-05-26 17:12:34 +0200

    Now truly sends to the server in batches (and not a doc at a time)

--- src/main/scala/com/github/takezoe/solr/scala/BatchRegister.scala
+++ src/main/scala/com/github/takezoe/solr/scala/BatchRegister.scala
@@ -3,23 +3,40 @@ package com.github.takezoe.solr.scala
 import org.apache.solr.client.solrj.response.UpdateResponse
 import org.apache.solr.client.solrj.{SolrClient => ApacheSolrClient}
 import org.apache.solr.common.SolrInputDocument
+import collection.JavaConverters._
 
 class BatchRegister(server: ApacheSolrClient, collection: Option[String], docs: Map[String, Any]*) {
   add(docs: _*)
 
   def add(docs: Any*): BatchRegister = {
-    CaseClassMapper.toMapArray(docs: _*).foreach { doc =>
+    val solrDocs = CaseClassMapper.toMapArray(docs: _*).map { doc =>
       val solrDoc = new SolrInputDocument
       doc.collect { case (key, value) =>
         solrDoc.addField(key, value)
       }
-      collection match {
-        case Some(c) => server.add(c, solrDoc)
-        case None => server.add(solrDoc)
+      solrDoc
+    }.toSeq
+
+    collection match {
+        case Some(c) => server.add(c, solrDocs.asJava)
+        case None => server.add(solrDocs.asJava)
       }
-    }
     this
   }
+
+  // def add(docs: Any*): BatchRegister = {
+  //   CaseClassMapper.toMapArray(docs: _*).foreach { doc =>
+  //     val solrDoc = new SolrInputDocument
+  //     doc.collect { case (key, value) =>
+  //       solrDoc.addField(key, value)
+  //     }
+  //     collection match {
+  //       case Some(c) => server.add(c, solrDoc)
+  //       case None => server.add(solrDoc)
+  //     }
+  //   }
+  //   this
+  // }
 
   def commit(): UpdateResponse = server.commit
 
